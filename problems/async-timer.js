a01e82b84150a7f40014a6b8f76b3542824ddc78
Sunil kumar <sunilsmail.amujuri@gmail.com>
2024-01-03 07:50:09 +0530

    Update problems

---
+++ problems/async-timer.js
@@ -0,0 +1,42 @@
+4- function delay(user){
+
+5-
+
+
+Email sent to sneha@gmail.com Email sent to sam@gmail.com
+
+return new Promise(resolve
+
+Email sent to all the users...
+
+setTimeout(function(){
+
+resolve( Email sent to ${user}"); . 1000);
+
+13- async function sendEmail(){
+
+14- for(user of users){
+
+15
+
+16
+
+17
+
+18
+
+const confirmation await delay(user):
+
+console.log(confirmation);
+
+}
+
+console.log('Email sent to all the users...
+
+19)
+
+20
+
+21
+
+22 sendEmail():
