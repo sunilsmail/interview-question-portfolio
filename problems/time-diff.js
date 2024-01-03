0991a0896e40bf452dd0d2e5a2a46e1e93b22bc1
Sunil kumar <sunilsmail.amujuri@gmail.com>
2024-01-03 07:12:58 +0530

    Update time-diff.js

--- problems/time-diff.js
+++ problems/time-diff.js
@@ -9,22 +9,21 @@ function timeDifference(startTimestamp, endTimestamp) {
   if (seconds < 60) {
     return `${seconds} sec`;
   } else if (minutes < 60) {
-    return `${minutes} min`;
+    return `${minutes} min ago`;
   } else if (hours < 24) {
-    return `${hours} hr`;
+    return `${hours} hr ago`;
   } else {
-    return `${days} days`;
+    return `${days} days ago`;
   }
 }
 
 // Example usage:
-const startTimestamp = new Date("2024-01-03T10:00:00").getTime();
-const endTimestamp = new Date("2024-01-03T15:30:00").getTime();
-
+const timeList = ["07:16:08", "07:16:25", "07:15:45", "07:14:00"];
+const startTimestamp = new Date().getTime();
+const time = timeList[0].split(':')
+const endTimestamp = new Date(`2024-01-03T${time[0]}:${time[1]}:${time[2]}`).getTime();
+console.log(startTimestamp)
 const timeDifferenceMessage = timeDifference(startTimestamp, endTimestamp);
 console.log(`Time difference: ${timeDifferenceMessage}`);
 
-
-const currentTime = new Date().toLocaleTimeString("en-US", { hour12: false });
-const timeList = ["07:16:08", "07:16:25", "07:15:45", "07:14:00"];
 
