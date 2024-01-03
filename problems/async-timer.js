e058eae7efd264d67ae20b64f1125ce331d4720b
Sunil kumar <sunilsmail.amujuri@gmail.com>
2024-01-03 08:10:42 +0530

    Update async-timer.js

--- problems/async-timer.js
+++ problems/async-timer.js
@@ -1,29 +1,26 @@
+const users =["nisha@gmail.com", "sneha@gmail.com", "sam@gmail.com"];
 
-const users ["nisha@gmail.com", "sneha@gmail.com", "sam@gmail.com");
+
 function delay(user){
 
-return new Promise(resolve
+return new Promise(resolve =>
 
 setTimeout(function(){
 
-resolve( Email sent to ${user}");  1000);
+resolve(`Email sent to ${user}`)
 
-})
+}, 1000))
 
 }
+
 
 async function sendEmail(){
 
 for(user of users){
-
-const confirmation await delay(user):
-
-console.log(confirmation);
-
+  const confirmation = await delay(user)
+  console.log(confirmation)
+}
+console.log('Email sent to all the users...')
 }
 
-console.log('Email sent to all the users...)
-
-}
-
-sendEmail():
+sendEmail()
